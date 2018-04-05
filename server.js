7a98f788ac2c13c166d399ff7a1a454e3b907d24
Will <fwgumbs@gmail.com>
2018-04-05 15:50:34 -0400

    cb

--- server.js
+++ server.js
@@ -23,8 +23,8 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 // Import routes and give the server access to them.
 
-var routes = require("./controllers/burgers_Controller.js");
-app.use("/", routes);
+var routes = require("controllers/burgers_Controller.js");
+app.use( routes);
 // Start our server so that it can begin listening to client requests.
 
 app.listen(PORT, function() {
